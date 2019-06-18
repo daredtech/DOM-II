c228406e10cb1dc32cc22ae41ef91da4d8ba8268
Red <vhanchar@ucsc.edu>
2019-06-17 21:59:17 -0700

    mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+let navTitle = document.getElementsByClassName('nav-link');
+let navTitleArray = Array.from(navTitle);
+
+//#
+navTitleArray .forEach(element => {
+    element.addEventListener('mouseover', (event) => {event.target.style.color = "orange"});
+    element.addEventListener('mouseout', (event) => {event.target.style.color = "black"});
+});
+
+
+
+
+
